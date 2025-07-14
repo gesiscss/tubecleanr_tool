4e142759afb18cc2ddd30d2b54c25323e40d2bef
Johannes Breuer <johannes.breuer@gesis.org>
2025-07-14 17:01:07 +0200

    First version of required materials

--- install.R
+++ install.R
@@ -1,25 +1,3 @@
-# install.R
-
-# Install remotes if not already installed
-if (!requireNamespace("remotes", quietly = TRUE)) {
-  install.packages("remotes")
-}
-
-# Install CRAN packages
-cran_packages <- c("ggplot2", "dplyr")
-for (pkg in cran_packages) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    install.packages(pkg)
-  }
-}
-
-# Install GitHub packages
-github_packages <- c(
-  "gesiscss/tubecleanR",
-  "gojiplus/tuber",
-  "vosonlab/vosonSML"
-)
-
-for (repo in github_packages) {
-  remotes::install_github(repo, upgrade = "never")
-}
+install.packages("tidyverse")
+install.packages("remotes")
+remotes::install_github("gesiscss/tubecleanR", upgrade = "never")
